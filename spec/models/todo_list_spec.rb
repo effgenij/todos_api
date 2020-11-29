4fc4297998dc8e9d916a9b046cf91a7042e26746
effgenij <effgenij@gmail.com>
2020-11-29 23:35:45 +0300

    add model tests

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe TodoList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  it { should have_many(:todo_items).dependent(:destroy) }
+  # Validation test
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:created_by) }
 end
